a91a3a6d2253b4e4560451cf718d22ed54daae30
Zhang Ximin <zhangximin@gmail.com>
2018-04-13 12:42:37 +0800

    Add missing package.

--- Prepare_toolchain.sh
+++ Prepare_toolchain.sh
@@ -10,8 +10,8 @@ fi
 apt-get -y --no-install-recommends --fix-missing install \
 	bsdtar mtools u-boot-tools pv bc \
 	gcc automake make \
-	lib32z1 lib32z1-dev qemu-user-static \
-	dosfstools libncurses5-dev lib32stdc++ debootstrap
+	lib32z1 lib32z1-dev binfmt-support qemu-user-static \
+	dosfstools libncurses5-dev lib32stdc++6 debootstrap
 
 # Prepare toolchains
 ROOT=`cd .. && pwd`
